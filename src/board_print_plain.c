07cf3ad029beb1bf8fd742e85f0b71f9a655aadc
MishaDak <dack.misha@gmail.com>
2019-04-02 12:54:27 +0700

    Extract printing desk to file

---
+++ src/board_print_plain.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "board_print_plain.h"
+
+extern char numbers[8];
+extern char desk[8][8];
+
+void printdesk()
+{
+    int i, j;
+    for (i = 0; i < 8; i++) {
+        printf("%d ", numbers[i]);
+        for (j = 0; j < 8; j++) {
+            printf("%c ", desk[i][j]);
+        }
+        printf("\n");
+    }
+    printf("  ");
+    for (i = 0; i < 8; i++)
+        printf("%c ", i + 97);
+    printf("\n");
+}
