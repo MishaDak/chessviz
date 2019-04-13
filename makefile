51494323e45f77e798173abe673d4e4fbeeca404
MishaDak <dack.misha@gmail.com>
2019-04-14 01:04:00 +0700

    Change Makefile

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: bin/prog
 
-bin/prog: build/main.o build/board_print_plain.o
-	gcc -Wall -Werror build/main.o build/board_print_plain.o -o bin/prog
+bin/prog: build/main.o build/board_print_plain.o build/board.o
+	gcc -Wall -Werror build/main.o build/board_print_plain.o build/board.o -o bin/prog
 
 build/main.o: src/main.c
 	gcc -Wall -Werror -c src/main.c -o build/main.o
@@ -9,6 +9,9 @@ build/main.o: src/main.c
 build/board_print_plain.o: src/board_print_plain.c
 	gcc -Wall -Werror -c src/board_print_plain.c -o build/board_print_plain.o
 
+build/board.o: src/board.c
+	gcc -Wall -Werror -c src/board.c -o build/board.o
+	
 .PHONY: clean
 
 clean:
--- src/board.c
+++ src/board.c
@@ -3,84 +3,86 @@
 #include <string.h>
 
 extern chartoint(char input[5]);
-int x1,y2,y1,Yy;
+int x1, y2, y1, Yy;
 
-void scanan(int flag)
-{
-    char input[7] = 'NULL';
+void scanan(int flag) {
+  char input[7] = 'NULL';
+  while (1) {
     while (1) {
-        while(1) {
-            fgets(input, 7, stdin);
-            if (chartoint(input)) {
-                break;
-            }
-            printf("Введите заново: ")ж
-        }
-        if (flag == 1) {
-            if (white() == 1) {
-                break;
-            } else {
-                printf("Введите заново: ")
-            }
-        }
-        if (flag == 2) {
-            if (black() == 1) {
-                break;
-            } else {
-                printf("Введите заново: ")
-            }
-        }
+      fgets(input, 7, stdin);
+      if (chartoint(input)) {
+        break;
+      }
+      printf("Введите заново: ") ж
     }
+    if (flag == 1) {
+      if (white() == 1) {
+        break;
+      } else {
+        printf("Введите заново: ")
+      }
+    }
+    if (flag == 2) {
+      if (black() == 1) {
+        break;
+      } else {
+        printf("Введите заново: ")
+      }
+    }
+  }
 }
 
-int chartoint(char input[7])
-{
-    x1 = (int)input[0] - 'A';
-    y1 = (int)input[1] - '1';
-    x2 = (int)input[3] - 'A';
-    y2 = (int)input[4] - '1';
-    if ((x2 < 8) && (x2 >= 0) && (y2 >= 0) && (y2 < 8)
-        && (x1 >= 0) && (x1 < 8) && (y1 >= 0) && (y1 < 8))
-        return 1;
-    return 0;
+int chartoint(char input[7]) {
+  x1 = (int)input[0] - 'A';
+  y1 = (int)input[1] - '1';
+  x2 = (int)input[3] - 'A';
+  y2 = (int)input[4] - '1';
+  if ((x2 < 8) && (x2 >= 0) && (y2 >= 0) && (y2 < 8) && (x1 >= 0) && (x1 < 8) &&
+      (y1 >= 0) && (y1 < 8))
+    return 1;
+  return 0;
 }
 
-int white(){
+int white() {
     if (desk[y1][x1] > 64 && desk[y1][x1 <73) {
-        return 0; //Не рубим свои фигуры
+      return 0; //Не рубим свои фигуры
     }
     switch (desk[y1][x1]) {
     case 'P':
-        if ((desk[y2][x2] == ' ') && (y1 == 1) && (x1 == x2) && (y2 - y1 > 0) && (y2 - y1 < 3)) {
-            return 1; //Первый ход
-        }
-        if ((desk[y2][x2] == ' ') && (x2 == x1) && (y2 - y1 == 1)) {
-            return 1; //Ход по пустым клеткам
-        }
-        if ((desk[y2][x2] > 'a' && desk[y2][x2] < 's') && ((x2 - x1 == 1) || (x2 - x1 == -1)) && (y2 - y1 == 1)) {
-            return 1; // Рубим фигуры опонента
-        }
-        return 0;
-    }
-}
-
-int black(){
-    if (desk[y1][x1] > 96 && desk[y1][x1] < 105) {
-        return 0;
-    }
-    switch (desk[y1][x1]) {
-    case 'p':
-        if ((desk[y2][x2] == ' ') && (y1 == 6) && (x1 == x2) && (y1 - y2 > 0) && (y1 - y2 < 3)) {
-            return 1 //Первый ход 
-        }
-        if ((desk[y2][x2] == ' ') && (x2 == x1) && (y1 -y2 == 1)) {
-            return 1; //Ход по пустым клеткам
-        }
-        if ((desk[y2][x2] < 'S' && desk[y2][x2] > 'A') && ((x1 - x2 == 1) || (x1 -x2 == -1)) && (y1 - y2 == 1)) {
-            return 1;
-        }
+      if ((desk[y2][x2] == ' ') && (y1 == 1) && (x1 == x2) && (y2 - y1 > 0) &&
+          (y2 - y1 < 3)) {
+        return 1; //Первый ход
+      }
+      if ((desk[y2][x2] == ' ') && (x2 == x1) && (y2 - y1 == 1)) {
+        return 1; //Ход по пустым клеткам
+      }
+      if ((desk[y2][x2] > 'a' && desk[y2][x2] < 's') &&
+          ((x2 - x1 == 1) || (x2 - x1 == -1)) && (y2 - y1 == 1)) {
+        return 1; // Рубим фигуры опонента
+      }
     }
     return 0;
+}
+
+int black() {
+  if (desk[y1][x1] > 96 && desk[y1][x1] < 105) {
+    return 0;
+  }
+  switch (desk[y1][x1]) {
+  case 'p':
+    if ((desk[y2][x2] == ' ') && (y1 == 6) && (x1 == x2) && (y1 - y2 > 0) &&
+        (y1 - y2 < 3)) {
+      return 1 //Первый ход
+    }
+    if ((desk[y2][x2] == ' ') && (x2 == x1) && (y1 - y2 == 1)) {
+      return 1; //Ход по пустым клеткам
+    }
+    if ((desk[y2][x2] < 'S' && desk[y2][x2] > 'A') &&
+        ((x1 - x2 == 1) || (x1 - x2 == -1)) && (y1 - y2 == 1)) {
+      return 1;
+    }
+  }
+  return 0;
 }
 
 void move(){
