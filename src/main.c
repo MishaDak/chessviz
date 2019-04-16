3c1879abcfa96e03aa0d7ea4a2ac2d11d7adccf0
MishaDak <dack.misha@gmail.com>
2019-04-16 12:43:30 +0700

    Code style

--- src/main.c
+++ src/main.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
 #include "board.h"
 #include "board_print_plain.h"
+#include <stdio.h>
 
 int main()
 {
-  printdesk();
-  while (1) {
-      printf("Белые фигуры(Заглавные буквы): ");
-      scanan(1);
-      move();  
-      printdesk();
-      printf("Чёрные фигуры(Строчные буквы)");
-      scanan(2);
-      move();
-      printdesk();
-  }  
+    printdesk();
+    while (1) {
+        printf("Белые (Большие буквы):");
+        scanan(1);
+        move();
+        printdesk();
+        printf("Черные (Маленькие буквы):");
+        scanan(2);
+        move();
+        printdesk();
+        break;
+    }
     return 0;
 }
